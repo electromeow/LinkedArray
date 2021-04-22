c6ad1cdca1d2673b6f07fbe43ff0c813f499515f
electromeow <gokgoz.berat123@gmail.com>
2021-04-22 11:06:46 +0300

    Merge sort added.

--- src/algorithms.ts
+++ src/algorithms.ts
@@ -24,4 +24,43 @@ function quick_sort(list: LinkedArray): LinkedArray {
   return a;
 }
 
+function merge_sort(listinput: LinkedArray): LinkedArray {
+  const list = new LinkedArray();
+  listinput.toArray().forEach((element) => {
+    list.push(element);
+  }); //copied the LinkedArray
+  if (list.length < 2) return list;
+  let mid = list.length;
+  mid = (mid - (mid % 2)) / 2;
+  let side1 = list.slice(0, mid);
+  let side2 = list.slice(mid, list.length);
+  side1 = merge_sort(side1);
+  side2 = merge_sort(side2);
+  let i = 0;
+  let j = 0;
+  let k = 0;
+  while (i < side1.length && j < side2.length) {
+    if (side1.get(i) < side2.get(j)) {
+      list.set(k, side1.get(i));
+      i++;
+    } else {
+      list.set(k, side2.get(j));
+      j++;
+    }
+    k++;
+  }
+  while (i < side1.length) {
+    list.set(k, side1.get(i));
+    i++;
+    k++;
+  }
+  while (j < side2.length) {
+    list.set(k, side2.get(j));
+    j++;
+    k++;
+  }
+  return list;
+}
+
+exports.merge_sort = merge_sort;
 exports.quick_sort = quick_sort;
