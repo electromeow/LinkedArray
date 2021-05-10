a8edbd4242eb0deba723fcf660148d1bab9b6e1b
electromeow <gokgoz.berat123@gmail.com>
2021-05-10 07:15:51 +0300

    binary search completed

--- src/algorithms.ts
+++ src/algorithms.ts
@@ -1,8 +1,10 @@
 import { LinkedArray } from "./classes";
 
-function quick_sort(list: LinkedArray): LinkedArray {
+function quick_sort(unsorted_list: LinkedArray): any {
+  var list = new LinkedArray();
+  list = Object.assign(list, unsorted_list);
   const len: number = list.length;
-  let pivot;
+  let pivot: any;
   if (len <= 1) {
     return list;
   } else {
@@ -10,17 +12,30 @@ function quick_sort(list: LinkedArray): LinkedArray {
   }
   const biggers = new LinkedArray();
   const smallers = new LinkedArray();
-  for (let i = 0; i < list.length; i++) {
-    const x = list.get(i);
+  list.forEach((x) => {
     if (typeof x !== "number") throw new Error("Only numbers can be sorted.");
     if (x > pivot) biggers.push(x);
     else smallers.push(x);
+  });
+  var a: LinkedArray;
+  var b: LinkedArray;
+  if (smallers.get(0) === undefined) {
+    a = new LinkedArray(pivot);
+    b = quick_sort(biggers);
+    b.forEach((x) => a.push(x));
+    return a;
+  } else if (biggers.get(0) === undefined) {
+    b = new LinkedArray(pivot);
+    a = quick_sort(smallers);
+    b.forEach((x) => a.push(x));
+    return a;
+  } else {
+    a = quick_sort(smallers);
+    b = quick_sort(biggers);
+    a.push(pivot);
+    b.forEach((x) => a.push(x));
+    return a;
   }
-  const a = quick_sort(smallers);
-  const b = quick_sort(biggers);
-  a.push(pivot);
-  for (let i = 0; i < b.length; i++) a.push(b.get(i));
-  return a;
 }
 
 function merge_sort(listinput: LinkedArray): LinkedArray {
@@ -60,24 +75,45 @@ function merge_sort(listinput: LinkedArray): LinkedArray {
   }
   return list;
 }
-/*
-function binary_search(list: LinkedArray, val: number, sort = false): number {
-  let value: number;
-  if (sort) {
-    list = quick_sort(list);
-  }
 
-  const len = list.length;
+function binary_search(
+  list: LinkedArray,
+  val: any,
+  sort = false,
+  indexRange: number[]
+): number {
+  if (typeof val !== "number")
+    throw new Error("Only numbers can be searched using binary search.");
+  if (!LinkedArray.isLinkedArray(list))
+    throw new Error("List given must be a LinkedArray Object");
+  if (sort) {
+    var list1: LinkedArray = quick_sort(list);
+  } else {
+    var list1: LinkedArray = new LinkedArray();
+    list1 = Object.assign(list1, list);
+  }
+  const len = list1.length;
+  if (!indexRange) indexRange = [0, len];
+  if (len <= 1 && list1.get(0) === val) return indexRange[0];
+  if (len <= 1 && list1.get(0) !== val)
+    throw new Error("Value couldn't found!");
   const mid = (len - (len % 2)) / 2;
-  const side1 = list.slice(0, mid);
-  const side2 = list.slice(mid, len);
-  if (side1.get(mid - 1) > val) {
-    return binary_search(side1, val);
-  } else if (side1.get(mid - 1) < val) {
-    return binary_search(side2, val);
-  } else return list.get(mid - 1);
+  const side1 = list1.slice(0, mid);
+  const side2 = list1.slice(mid, len);
+  if (val < side2.get(0))
+    return binary_search(side1, val, false, [
+      indexRange[0],
+      Math.floor((indexRange[1] - indexRange[0]) / 2) + indexRange[0],
+    ]);
+  else if (val === side2.get(0))
+    return Math.floor((indexRange[1] - indexRange[0]) / 2) + indexRange[0];
+  else
+    return binary_search(side2, val, false, [
+      Math.floor((indexRange[1] - indexRange[0]) / 2) + indexRange[0],
+      indexRange[1],
+    ]);
 }
-*/
+
 exports.merge_sort = merge_sort;
 exports.quick_sort = quick_sort;
-//exports.binary_search = binary_search;
+exports.binary_search = binary_search;
